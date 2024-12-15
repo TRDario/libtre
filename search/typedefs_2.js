49eff05b2a4f4650976e85e739994fdfe9b86d1a
TRDario <TRDario@users.noreply.github.com>
2024-12-15 22:44:22 +0000

    deploy: 08ac1dbe7cdbca883795b2cba6eb3d14dc878fde

--- search/typedefs_2.js
+++ search/typedefs_2.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['texturedquad_0',['TexturedQuad',['../classtre_1_1Renderer2D.html#abcb4d483afa162522e6ea9d7a2b379c1',1,'tre::Renderer2D']]],
-  ['texturedtriangle_1',['TexturedTriangle',['../classtre_1_1Renderer2D.html#a292a32d1b2776cba7d9601bc53b95e91',1,'tre::Renderer2D']]],
-  ['texturedvertexfan_2',['TexturedVertexFan',['../classtre_1_1Renderer2D.html#ac77775e6fa11bf5fadde76a167390341',1,'tre::Renderer2D']]],
-  ['textureref_3',['TextureRef',['../classtre_1_1Renderer2D.html#a08cc0b709b4bbc3eba30d161e4ed635a',1,'tre::Renderer2D']]]
+  ['texturedvertexfan_1',['TexturedVertexFan',['../classtre_1_1Renderer2D.html#ac77775e6fa11bf5fadde76a167390341',1,'tre::Renderer2D']]],
+  ['textureref_2',['TextureRef',['../classtre_1_1Renderer2D.html#a08cc0b709b4bbc3eba30d161e4ed635a',1,'tre::Renderer2D']]]
 ];
